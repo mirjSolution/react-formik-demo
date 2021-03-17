6e99c65513c7cdedd6641edf20967f55b4fa2b3e
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-17 15:19:03 -0400

    Handling form submission

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -8,13 +8,14 @@ function YoutubeForm() {
       email: '',
       channel: '',
     },
+    onSubmit: (values) => {
+      console.log('Form data', values);
+    },
   });
-
-  console.log('Form values', formik.values);
 
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor='name'>Name</label>
         <input
           type='text'
@@ -42,7 +43,7 @@ function YoutubeForm() {
           value={formik.values.channel}
         />
 
-        <button>Submit</button>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
