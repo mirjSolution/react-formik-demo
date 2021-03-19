f9eab2027ca65f0fcd940eb1a98959e4b5f24e65
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-18 20:27:11 -0400

    Enrollment Form using the reusable components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginForm from './components/LoginForm';
 
 import './App.css';
 import RegistrationForm from './components/RegistrationForm';
+import EnrollmentForm from './components/EnrollmentForm';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <YoutubeForm /> */}
       {/* <FormikContainer /> */}
       {/* <LoginForm /> */}
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
+      <EnrollmentForm />
     </div>
   );
 }
